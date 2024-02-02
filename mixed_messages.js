09475952c451e884fb269cc5803fd661b5809155
LAIN-21 <linfante.ieu2023@student.ie.edu>
2024-02-02 19:39:22 +0100

    Finished Product

--- mixed_messages.js
+++ mixed_messages.js
@@ -1,1 +1,14 @@
+let firstWords = ['I', 'You', 'He', 'She', 'It', 'We', 'They'];
+let secondWords = ['will', 'shall', 'may', 'can', 'must', 'ought to', 'might'];
+let thirdWords = ['eat', 'drink', 'sleep', 'run', 'walk', 'talk', 'sing', 'dance', 'play', 'work'];
+let lastWord = 'in this life.';
 
+const wordBuilder = (arr1, arr2, arr3, last) => {
+    let first = arr1[Math.floor(Math.random() * arr1.length)];
+    let second = arr2[Math.floor(Math.random() * arr2.length)];
+    let third = arr3[Math.floor(Math.random() * arr3.length)];
+    let fourth = last[Math.floor(Math.random() * last.length)];
+    return `${first} ${second} ${third} ${fourth}`;
+}
+
+console.log(wordBuilder(firstWords, secondWords, thirdWords, lastWord));
